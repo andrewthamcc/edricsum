59bb1a5f84130686ce43d523c1a51006f8ad8e58
Andrew Tham <andrewtham@Andrews-MacBook-Pro-2.local>
2026-08-13 22:03:51 -0400

    navigation and information

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import netlify from "@astrojs/netlify";
 
+import icon from "astro-icon";
+
 export default defineConfig({
   output: "server",
   adapter: netlify(),
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [icon()],
 });
